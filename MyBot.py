5c3bea034e50937b33ccfa28f6ab69cad400ec7f
Alastair Payne <alastairpayne@gmail.com>
2016-11-20 21:11:33 +0000

    V2

--- MyBot.py
+++ MyBot.py
@@ -2,7 +2,40 @@ from hlt import *
 from networking import *
 
 myID, gameMap = getInit()
-sendInit("MyPythonBot")
+sendInit("MyPythonBotXXX")
+
+def move(location):
+    site = gameMap.getSite(location)
+    weakestBeatableForeignDirection = weakestBeatableForeignNeighbour(location)
+
+    if weakestBeatableForeignDirection != 0:
+        weakestBFSite = gameMap.getSite(location, weakestBeatableForeignDirection)
+        if weakestBFSite.strength < site.strength:
+            return Move(location, weakestBeatableForeignDirection)
+
+    if allFriendly(location):
+        return Move(location, WEST)
+
+    return Move(location,STILL)
+
+def allFriendly(location):
+    allFriend = True;
+    for d in CARDINALS:
+        neighbour_site = gameMap.getSite(location,d)
+        if neighbour_site.owner != myID:
+            allFriend = False;
+    return allFriend
+
+def weakestBeatableForeignNeighbour(location):
+    strength = 255
+    direction = 0
+    for d in CARDINALS:
+        neighbour_site = gameMap.getSite(location,d)
+        if neighbour_site.strength <= strength and myID != neighbour_site.owner:
+            strength = neighbour_site.strength
+            direction = d;
+    return direction;
+
 
 while True:
     moves = []
@@ -11,5 +44,5 @@ while True:
         for x in range(gameMap.width):
             location = Location(x, y)
             if gameMap.getSite(location).owner == myID:
-                moves.append(Move(location, random.choice(DIRECTIONS)))
+                moves.append(move(location))
     sendFrame(moves)
