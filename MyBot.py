387d3deec764ba719152d2d6fc806cd5b01911db
Alastair Payne <alastairpayne@gmail.com>
2016-11-21 01:27:13 +0000

    v3, setting enemy as v2

--- MyBot.py
+++ MyBot.py
@@ -2,21 +2,87 @@ from hlt import *
 from networking import *
 
 myID, gameMap = getInit()
+PRODUCTION_FACTOR = 5
 sendInit("MyPythonBotXXX")
 
-def move(location):
+def move(location, x, y, gameMap):
     site = gameMap.getSite(location)
     weakestBeatableForeignDirection = weakestBeatableForeignNeighbour(location)
 
     if weakestBeatableForeignDirection != 0:
         weakestBFSite = gameMap.getSite(location, weakestBeatableForeignDirection)
-        if weakestBFSite.strength < site.strength:
+        if weakestBFSite.strength < site.strength and site.strength >= site.production * PRODUCTION_FACTOR:
             return Move(location, weakestBeatableForeignDirection)
 
+    #this bit is shit
     if allFriendly(location):
-        return Move(location, WEST)
+        #return Move(location, randomNW())
+        return Move(location, directionOfNearestUnfriendlySquare(x,y,gameMap))
 
     return Move(location,STILL)
+
+def randomNW():
+    if random.random() < 0.5 :
+        return NORTH
+    else:
+        return WEST
+
+def directionOfNearestUnfriendlySquare(x, y, gameMap):
+    directionCounts = [0,0,0,0]
+    localX = 0
+    localY = 0
+    for d in CARDINALS:
+        # if d == EAST:
+        #     while localX <= gameMap.width:
+        #         neighbour_site = gameMap.getSite(Location((x+localX) % gameMap.width, y),d)
+        #         localX +=1
+        #         break
+        # if d == WEST:
+        #     c=1
+        # if d == NORTH:
+        #     c=1
+        # if d == SOUTH:
+        #     c=1
+        #
+        if d == EAST:
+            while localX <= gameMap.width:
+                neighbour_site = gameMap.getSite(Location((x+localX) % gameMap.width, y),d)
+                if neighbour_site.owner == myID:
+                    directionCounts[d-1]+=1
+                    localX+=1
+                else:
+                    localX = 0;
+                    break
+        if d == WEST:
+            while localX <= gameMap.width:
+                neighbour_site = gameMap.getSite(Location((x-localX) % gameMap.width, y),d)
+                if neighbour_site.owner == myID:
+                    directionCounts[d-1]+=1
+                    localX+=1
+                else:
+                    localX = 0
+                    break
+        if d == NORTH:
+            while localY <= gameMap.height:
+                neighbour_site = gameMap.getSite(Location(x, (y - localY) % gameMap.height),d)
+                if neighbour_site.owner == myID:
+                    directionCounts[d-1]+=1
+                    localY+=1
+                else:
+                    localY = 0
+                    break
+        if d == SOUTH:
+            while localY <= gameMap.height:
+                neighbour_site = gameMap.getSite(Location(x, (y+localY) % gameMap.height),d)
+                if neighbour_site.owner == myID:
+                    directionCounts[d-1]+=1
+                    localY+=1
+                else:
+                    localY=0
+                    break
+
+    return directionCounts.index(min(directionCounts)) + 1
+
 
 def allFriendly(location):
     allFriend = True;
@@ -44,5 +110,5 @@ while True:
         for x in range(gameMap.width):
             location = Location(x, y)
             if gameMap.getSite(location).owner == myID:
-                moves.append(move(location))
+                moves.append(move(location,x,y,gameMap))
     sendFrame(moves)
