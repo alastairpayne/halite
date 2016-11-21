2d287cb96d551974f1932c43a9bbefc3e3dd0a02
Alastair Payne <alastairpayne@gmail.com>
2016-11-21 03:32:25 +0000

    spaces in the middle of friendly territory are now more sensible

--- MyBot.py
+++ MyBot.py
@@ -2,7 +2,10 @@ from hlt import *
 from networking import *
 
 myID, gameMap = getInit()
+START_POS = [0,0]
+INIT = False
 PRODUCTION_FACTOR = 3
+GIVE_UP_ON_THAT_LONG_FUNCTION_FACTOR = 3
 sendInit("MyPythonBotXXX")
 
 def move(location, x, y, gameMap):
@@ -29,6 +32,8 @@ def randomNW():
     else:
         return WEST
 
+#I can use my starting square to help with this right?
+#just keep going in the same direction
 def directionOfNearestUnfriendlySquare(x, y, gameMap):
     directionCounts = [0,0,0,0]
     localX = 0
@@ -43,7 +48,7 @@ def directionOfNearestUnfriendlySquare(x, y, gameMap):
                 else:
                     localX = 0;
                     break
-                if localX > 3:
+                if localX > GIVE_UP_ON_THAT_LONG_FUNCTION_FACTOR:
                     break;
         if d == WEST:
             while localX <= gameMap.width:
@@ -54,7 +59,7 @@ def directionOfNearestUnfriendlySquare(x, y, gameMap):
                 else:
                     localX = 0
                     break
-                if localX > 3:
+                if localX > GIVE_UP_ON_THAT_LONG_FUNCTION_FACTOR:
                     break;
         if d == NORTH:
             while localY <= gameMap.height:
@@ -65,7 +70,7 @@ def directionOfNearestUnfriendlySquare(x, y, gameMap):
                 else:
                     localY = 0
                     break
-                if localY > 3:
+                if localY > GIVE_UP_ON_THAT_LONG_FUNCTION_FACTOR:
                     break;
         if d == SOUTH:
             while localY <= gameMap.height:
@@ -76,10 +81,29 @@ def directionOfNearestUnfriendlySquare(x, y, gameMap):
                 else:
                     localY=0
                     break
-                if localY > 3:
+                if localY > GIVE_UP_ON_THAT_LONG_FUNCTION_FACTOR:
                     break;
 
+    if directionCounts[0] == directionCounts[1] and directionCounts[1] == directionCounts[2] and directionCounts[2] == directionCounts[3]:
+        return OkayJustGoBasedOnTheStartPosition(x,y)
+
     return directionCounts.index(min(directionCounts)) + 1
+
+def OkayJustGoBasedOnTheStartPosition(x,y):
+    startX = START_POS[0]
+    startY = START_POS[1]
+    diffX = abs(startX - x)
+    diffY = abs(startY - y)
+    if diffX > diffY:
+        if x > startX:
+            return EAST
+        else:
+            return WEST
+    else:
+        if y > startY:
+            return SOUTH
+        else:
+            return NORTH
 
 
 def allFriendly(location):
@@ -101,6 +125,7 @@ def weakestBeatableForeignNeighbour(location):
     return direction;
 
 
+#should I look for a first enemy to attack?
 while True:
     moves = []
     gameMap = getFrame()
@@ -108,5 +133,8 @@ while True:
         for x in range(gameMap.width):
             location = Location(x, y)
             if gameMap.getSite(location).owner == myID:
+                if (not INIT):
+                    INIT = True
+                    START_POS = [x,y]
                 moves.append(move(location,x,y,gameMap))
     sendFrame(moves)
